0d0c864979ea46a18ae80973b046a78d9c506ba8
RememberLenny <leonard_bogdonoff@condenast.com>
2014-11-25 18:59:10 -0500

    Create analyze points

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var Cardify = {};
 
+/**
+ * Analyze object
+ *
+ * The functions under Analyze will grab page content and analyze it.
+ *
+ * Specifically, the Analyze object will identify how much content needs
+ * to be Cardified. The content will be examined for word count, potential
+ * card breaks, and any assets requiring special layout.
+ *
+ * Eventually, Analyze will be fed content from Gather.
+ *
+ */
 Cardify.Analyze = {};
 
 Cardify.Construct = {};
